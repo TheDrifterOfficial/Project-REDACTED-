b3fa40540b906a5e1cf39a4900adb01a79b0cda0
TheDrifterOfficial <142444311+TheDrifterOfficial@users.noreply.github.com>
2024-12-08 18:52:38 -0500

    Update callstats.py

--- callstats.py
+++ callstats.py
@@ -1,4 +1,5 @@
 from delayed_print import*
+from class_armor_selection import*
 resistance=0
 durability=0
 socialism=0
@@ -6,23 +7,24 @@ stealth=0
 recovery=0
 player_class=str
 
-if player_class == "W":
-    resistance += 50
-    stealth -= 20
-    socialism += 30
-    recovery += 5
-else:
-    if player_class == "S":
-        resistance += 5
-        stealth += 0
-        socialism += 40
-        recovery += 50
+def calculate_stats():
+    if player_class == "W":
+        resistance += 50
+        stealth -= 20
+        socialism += 30
+        recovery += 5
     else:
-        if player_class == "P":
-            resistance += 0
-            stealth += 50
-            socialism += 0
-            recovery += 10
+        if player_class == "S":
+            resistance += 5
+            stealth += 0
+            socialism += 40
+            recovery += 50
+        else:
+            if player_class == "P":
+                resistance += 0
+                stealth += 50
+                socialism += 0
+                recovery += 10
 
 def call_stats():
     delay_print("\nHere are your statistics: Resistance = ")
