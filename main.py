1a6f747d5e0213e3daacd1c08b68c7f553d4553d
TheDrifterOfficial <142444311+TheDrifterOfficial@users.noreply.github.com>
2024-12-13 15:47:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-n="TheDrifter"
 from delayed_print import*
 from callstats import call_stats
+n="TheDrifter"
 
-delay_print("Hello adventurer, you have joined the magical world of [REDACTED], where anything can happen.")
-delay_print("\nThis is your first step in becoming, legend.")
-delay_print("\nBut first, any adventurer must be known by some special name. What is your name?")
+if __name__ == "__main__":
 
-delay_print("\nPlease input your new name. This name will be used to refer to you across your adventure:")
-n=str(input(" "))
-delay_print("Please confirm your character's name with Y (yes) or N (no): ")
-delay_print(n)
-c=str(input(": "))
-if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
-    delay_print("Please input your new name. This name will be used to refer to you across your adventure:")
+
+    global resi
+    global dura
+    global soci
+    global stea
+    global reco
+    resi=float(0)
+    dura=float(0)
+    soci=float(0)
+    stea=float(0)
+    reco=float(0)
+
+    delay_print("Hello adventurer, you have joined the magical world of [REDACTED], where anything can happen.")
+    delay_print("\nThis is your first step in becoming, legend.")
+    delay_print("\nBut first, any adventurer must be known by some special name. What is your name?")
+
+    delay_print("\nPlease input your new name. This name will be used to refer to you across your adventure:")
     n=str(input(" "))
-    delay_print("Please confirm that your character's with Y (yes) or N (no): ")
+    delay_print("Please confirm your character's name with Y (yes) or N (no): ")
     delay_print(n)
     c=str(input(": "))
     if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
@@ -36,14 +44,14 @@ if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or 
                 delay_print(n)
                 c=str(input(": "))
                 if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
-                    delay_print("Why? Why do do this to me. Just put in your name. Please!")
-                    delay_print("\nPlease input your new name. This name will be used to refer to you across your adventure:")
+                    delay_print("Please input your new name. This name will be used to refer to you across your adventure:")
                     n=str(input(" "))
                     delay_print("Please confirm that your character's with Y (yes) or N (no): ")
                     delay_print(n)
                     c=str(input(": "))
                     if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
-                        delay_print("Please input your new name. This name will be used to refer to you across your adventure:")
+                        delay_print("Why? Why do do this to me. Just put in your name. Please!")
+                        delay_print("\nPlease input your new name. This name will be used to refer to you across your adventure:")
                         n=str(input(" "))
                         delay_print("Please confirm that your character's with Y (yes) or N (no): ")
                         delay_print(n)
@@ -67,98 +75,89 @@ if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or 
                                     delay_print(n)
                                     c=str(input(": "))
                                     if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
-                                        delay_print("You know what? I am done. Screw this. I am calling you 'Dude'. I don't care if you like it or not.")
-                                        n = "Dude"
-delay_print("Well thats a great name, ")
-delay_print(n)
-delay_print(".")
-delay_print("\n\n\n\n\nSo, I guess you need some backstory.")
-delay_print("\nLong ago, a traveler came into the town of [REDACTED].")
-delay_print("\nShe promised riches and well-being for everyone.")
-delay_print("\nThere was only one condition.")
-delay_print("\nThat everyone would plead allegiance and loyalty to the traveler.")
-delay_print("\nSo, obviously, everyone did. They did whatever she asked and followed her blindly.")
-delay_print("\nShe turned out to be, one of them.")
-delay_print("\nNow the world needs your help, ")
-delay_print(n)
-delay_print(".")
+                                        delay_print("Please input your new name. This name will be used to refer to you across your adventure:")
+                                        n=str(input(" "))
+                                        delay_print("Please confirm that your character's with Y (yes) or N (no): ")
+                                        delay_print(n)
+                                        c=str(input(": "))
+                                        if c == "no" or c == "N" or c == "NO" or c == "No" or c == "n" or  c == "nn" or c == "oo" or c == "o" or c == "O" or c == "NN" or c == "OO" or c == "nah" or c == "Nah" or c == "NAh" or c == "NAH" or c == "nAH" or c == "naH":
+                                            delay_print("You know what? I am done. Screw this. I am calling you 'Dude'. I don't care if you like it or not.")
+                                            n = "Dude"
+    delay_print("Well thats a great name, ")
+    delay_print(n)
+    delay_print(".")
+    delay_print("\n\n\n\n\nSo, I guess you need some backstory.")
+    delay_print("\nLong ago, a traveler came into the town of [REDACTED].")
+    delay_print("\nShe promised riches and well-being for everyone.")
+    delay_print("\nThere was only one condition.")
+    delay_print("\nThat everyone would plead allegiance and loyalty to the traveler.")
+    delay_print("\nSo, obviously, everyone did. They did whatever she asked and followed her blindly.")
+    delay_print("\nShe turned out to be, one of them.")
+    delay_print("\nNow the world needs your help, ")
+    delay_print(n)
+    delay_print(".")
 
 
-from callstats import recovery
-from callstats import resistance
-from callstats import socialism
-from callstats import stealth
-from callstats import durability
-
-delay_print("\n\n\n\nAs part of you helping us, you will need some armour.")
-delay_print("\nPlease select your armour:")
-delay_print("\nIron Armour (+40 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+60 resistance, 4/10 durability)")
-delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
-a=str(input(""))
-if a == "I" or a == "L" or a == "C":
-    1+1
-else:
+    delay_print("\n\n\n\nAs part of you helping us, you will need some armour.")
     delay_print("\nPlease select your armour:")
-    delay_print("\nIron Armour (+50 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+50 resistance, 4/10 durability)")
+    delay_print("\nIron Armour (+40 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+60 resistance, 4/10 durability)")
     delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
     a=str(input(""))
     if a == "I" or a == "L" or a == "C":
-        1+1
+        pass
     else:
         delay_print("\nPlease select your armour:")
         delay_print("\nIron Armour (+50 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+50 resistance, 4/10 durability)")
         delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
         a=str(input(""))
         if a == "I" or a == "L" or a == "C":
-                1+1
+            pass
         else:
             delay_print("\nPlease select your armour:")
             delay_print("\nIron Armour (+50 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+50 resistance, 4/10 durability)")
             delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
             a=str(input(""))
             if a == "I" or a == "L" or a == "C":
-                1+1
+                    pass
             else:
                 delay_print("\nPlease select your armour:")
                 delay_print("\nIron Armour (+50 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+50 resistance, 4/10 durability)")
                 delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
                 a=str(input(""))
                 if a == "I" or a == "L" or a == "C":
-                    delay_print("\nYou have pissed me off, so now I am just giving you armour")
-                    a="A"
-if a == "I":
-        resistance=40
-        durability=7
-else:
-    if a == "L":
-        resistance+=30
-        durability+=10
+                    pass
+                else:
+                    delay_print("\nPlease select your armour:")
+                    delay_print("\nIron Armour (+50 resistance, 7/10 durability) / Leather Armour (+30 resistance, 10/10) / Chainmail Armour (+50 resistance, 4/10 durability)")
+                    delay_print("\nI for Iron, L for Leather, C for Chainmail: ")
+                    a=str(input(""))
+                    if a == "I" or a == "L" or a == "C":
+                        delay_print("\nYou have pissed me off, so now I am just giving you armour")
+                        a="A"
+    if a == "I":
+            resi=40
+            dura=7
     else:
-        if a == "C":
-            resistance+=60
-            durability+=4
+        if a == "L":
+            resi+=30
+            dura+=10
         else:
-            if a == "A":
-                resistance+=10
-                durability+=3
-delay_print("\n\nVery well, now it is time to pick your class.")
-delay_print("\nPlease select a class:")
-delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
-delay_print("\nScripters: Specializes in the arts of magic, casting different spells that grant benefits and punishments. Scripters are able to focus on multiple targets or one target. +5 Resistance, -40 Socialism, 0 Stealth, +50 Recovery.")
-delay_print("\nPythons: Specializes on stealth and maneuvering. Pythons are able to focus on multiple targets at once. 0 Resistance, 0 Socialism, +50 Stealth, +10 Recovery")
-delay_print("\nWarlords (W), Scripters (S), Pythons (P):")
-player_class=str(input(" "))
-if player_class == "W" or player_class =="S" or player_class == "P":
-    1+1
-else:
+            if a == "C":
+                resi+=60
+                dura+=4
+            else:
+                if a == "A":
+                    resi+=10
+                    dura+=3
+    delay_print("\n\nVery well, now it is time to pick your class.")
     delay_print("\nPlease select a class:")
     delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
     delay_print("\nScripters: Specializes in the arts of magic, casting different spells that grant benefits and punishments. Scripters are able to focus on multiple targets or one target. +5 Resistance, -40 Socialism, 0 Stealth, +50 Recovery.")
     delay_print("\nPythons: Specializes on stealth and maneuvering. Pythons are able to focus on multiple targets at once. 0 Resistance, 0 Socialism, +50 Stealth, +10 Recovery")
-    delay_print("\n Warlords (W), Scripters (S), Pythons (P)")
+    delay_print("\nWarlords (W), Scripters (S), Pythons (P):")
     player_class=str(input(" "))
     if player_class == "W" or player_class =="S" or player_class == "P":
-        1+1
+        pass
     else:
         delay_print("\nPlease select a class:")
         delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
@@ -167,7 +166,7 @@ else:
         delay_print("\n Warlords (W), Scripters (S), Pythons (P)")
         player_class=str(input(" "))
         if player_class == "W" or player_class =="S" or player_class == "P":
-            1+1
+            pass
         else:
             delay_print("\nPlease select a class:")
             delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
@@ -176,7 +175,7 @@ else:
             delay_print("\n Warlords (W), Scripters (S), Pythons (P)")
             player_class=str(input(" "))
             if player_class == "W" or player_class =="S" or player_class == "P":
-                1+1
+                pass
             else:
                 delay_print("\nPlease select a class:")
                 delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
@@ -185,45 +184,61 @@ else:
                 delay_print("\n Warlords (W), Scripters (S), Pythons (P)")
                 player_class=str(input(" "))
                 if player_class == "W" or player_class =="S" or player_class == "P":
-                    1+1
+                    pass
                 else:
-                    delay_print("\nVery well, you leave me no choice. You are now a Joker.")
-                    resistance=resistance-20
-                    stealth=stealth-20
-                    socialism=socialism-20
-                    recovery=recovery-20
-                    player_class="J"
-if player_class == "W":
-    resistance += 50
-    stealth -= 20
-    socialism += 30
-    recovery += 5
-else:
-    if player_class == "S":
-        resistance += 5
-        stealth += 0
-        socialism += 40
-        recovery += 50
+                    delay_print("\nPlease select a class:")
+                    delay_print("\nWarlords: Specializes on combat, specifically close combat. Warlords can focus on one target, dealing great damage. +50 Resistance, +30 Socialism, -20 Stealth, +5 Recovery.")
+                    delay_print("\nScripters: Specializes in the arts of magic, casting different spells that grant benefits and punishments. Scripters are able to focus on multiple targets or one target. +5 Resistance, -40 Socialism, 0 Stealth, +50 Recovery.")
+                    delay_print("\nPythons: Specializes on stealth and maneuvering. Pythons are able to focus on multiple targets at once. 0 Resistance, 0 Socialism, +50 Stealth, +10 Recovery")
+                    delay_print("\n Warlords (W), Scripters (S), Pythons (P)")
+                    player_class=str(input(" "))
+                    if player_class == "W" or player_class =="S" or player_class == "P":
+                        pass
+                    else:
+                        delay_print("\nVery well, you leave me no choice. You are now a Joker.")
+                        resi-=20
+                        stea-=20
+                        soci-=soci-20
+                        reco-=20
+                        player_class="J"
+    if player_class == "W":
+        resi += 50
+        stea -= 20
+        soci += 30
+        reco += 5
     else:
-        if player_class == "P":
-            resistance += 0
-            stealth += 50
-            socialism += 0
-            recovery += 10
-if player_class == "W":
-        player_class_name = "Warlord"
-else:
-    if player_class == "S":
-        player_class_name = "Scripter"
-    else:
-        if player_class == "P":
-            player_class_name = "Python"
+        if player_class == "S":
+            resi += 5
+            stea += 0
+            soci += 40
+            reco += 50
         else:
-            if player_class == "J":
-                player_class_name = "Joker"
-delay_print("\n")
-delay_print(n)
-delay_print(", you are now a ")
-delay_print(player_class_name)
+            if player_class == "P":
+                resi += 0
+                stea += 50
+                soci += 0
+                reco += 10
+    if player_class == "W":
+            player_class_name = "Warlord"
+    else:
+        if player_class == "S":
+            player_class_name = "Scripter"
+        else:
+            if player_class == "P":
+                player_class_name = "Python"
+            else:
+                if player_class == "J":
+                    player_class_name = "Joker"
+    delay_print("\n")
+    delay_print(n)
+    delay_print(", you are now a ")
+    delay_print(player_class_name)
 
-call_stats()
+recovery = int(reco)
+resistance = int(resi)
+durability = int(dura)
+socialism = int(soci)
+stealth = int(stea)
+
+if __name__ == "__main__":
+    call_stats()
